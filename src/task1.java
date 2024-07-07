3aea8012bcc810298c6bdc2c33afa3bbd266b661
Sviatlana <przhevals@gmail.com>
2024-07-07 23:33:18 +0200

    first commit

---
+++ src/task1.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //task 1 (min and max)
+
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int min = arr[0];
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+    }
+        
+    }
---
+++ src/task2.java
@@ -0,0 +1,16 @@
+public class task2 {
+    public static void main(String[] args) {
+        int[] arr = {11,12,13,14,15,16,17,18,19,20};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
